9b1ed09e14d31108905ec1b580b6e9f8127f6524
Syazwan Fitri <syazfitry69@gmail.com>
2021-12-30 13:55:45 +0800

    Added a slide animation for nav-links when clicked on hamburger icon

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger')
 const hamCloseIcon = document.querySelector('.hamburger-close');
+const navLinks = document.querySelector('.nav-links');
 
 let count = 0;
 hamburger.addEventListener('click', function() {
@@ -7,12 +8,14 @@ hamburger.addEventListener('click', function() {
     if(count === 0) {
         hamCloseIcon.src = './images/icon-close.svg';
         count = 1;
+        navLinks.classList.remove('display-none');
         console.log(count)
         return;
    }
     if(count === 1) {
         hamCloseIcon.src = './images/icon-hamburger.svg';
         count = 0;
+        navLinks.classList.add('display-none');
         console.log(count)
         return;
    }
